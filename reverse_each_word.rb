7a95657b8f697e4c665e3901e709f8b67e1174b3
Steve Alex <steve.alex@hotmail.co.uk>
2019-08-17 17:30:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
   new_string = ""
-  string.split.collect    new_string += word.reverse! + " "
+  string.split.collect
+  new_string += word.reverse! + " "
   end  
   new_string.strip!
 end
