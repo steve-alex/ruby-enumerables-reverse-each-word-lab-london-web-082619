d931d62873d3f5990d2309d04288e5b106efce88
Steve Alex <steve.alex@hotmail.co.uk>
2019-08-17 17:41:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 def reverse_each_word(string)
   new_string = []
   new_string << string.split
-  new_string.collect(|word| word.reverse!)
-  new_string = string.to_s
-  new_string
+  new_string.collect(|word| word.reverse)
 end
